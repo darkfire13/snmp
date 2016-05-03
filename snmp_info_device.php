fd6285a2d70cd8a02b8268c048bda20592b25b1a
Dmitriy <darkfire@dieg.info>
2016-05-03 13:18:48 +0300

    Key -c add

--- snmp_info_device.php
+++ snmp_info_device.php
@@ -1,15 +1,18 @@
 <?php
 
 /* Run script:
- * php snmp1.php -h 192.168.22.9
+ * php snmp_info_device.php -h 192.168.22.9
+ * php snmp_info_device.php -h 192.168.22.9 -c your_community
  */
 
 
 $community = 'public'; // default 'public'
-$opt = getopt("h:");
+$opt = getopt("h:c:");
+
+if (!empty($opt['c']))
+    $community = $opt['c'];
 
 echo "\nSNMPv2-MIB::sysContact.0 - Device contact details\n";
-//print_r(snmp2_real_walk($opt['h'], $community, "SNMPv2-MIB::sysContact.0"));
 echo(snmp2_get($opt['h'], $community, "SNMPv2-MIB::sysContact.0"));
 
 echo "\nSNMPv2-MIB::sysDescr.0 - Device description\n";
@@ -24,19 +27,23 @@ echo(snmp2_get($opt['h'], $community, "SNMPv2-MIB::sysName.0"));
 echo "\nSNMPv2-MIB::sysUpTime.0 - Device uptime\n";
 echo (snmp2_get($opt['h'], $community, "SNMPv2-MIB::sysUpTime.0"));
 
+// Interfaces
 echo "\nIF-MIB::ifNumber.0 - Number of network interfaces\n";
 echo (snmp2_get($opt['h'], $community, "IF-MIB::ifNumber.0"));
+
+echo "\nIF-MIB::ifDescr A textual string containing information about the interface\n";
+print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.2"));
+
+echo "\nIF-MIB::ifInOctets The total number of octets received on the interface, including framing characters.\n";
+print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.10"));
+
+echo "\nIF-MIB::ifOutOctets The total number of octets transmitted out of the interface, including framing characters.\n";
+print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.16"));
+
 
 //echo "\nIF-MIB::ifName - The textual name of the interface.\n";
 //print_r(snmp2_real_walk($opt['h'], $community, "IF-MIB::ifName"));
 //print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.31.1.1.1.1"));
-echo "\nIF-MIB::ifDescr информация об интерфейсах устройства\n";
-print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.2"));
-
-echo "\nIF-MIB::ifInOctets Полное число полученных байтов, включая символы заголовков.\n";
-print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.10"));
-echo "\nIF-MIB::ifOutOctets Полное количество отправленных октетов с интерфейса, включая символы заголовков.\n";
-print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.16"));
 
 //echo "\nMIBs of Private Enterprise Codes e.g. D-Link (171), Cisco(9), Microsoft (311). \n";
 //print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.4.1"));
@@ -45,7 +52,6 @@ print_r(snmp2_real_walk($opt['h'], $community, "1.3.6.1.2.1.2.2.1.16"));
 /*
   Ip Adress для DAP 2360 A1
   snmpwalk -v 2c -c luxorpub 192.168.22.9 1.3.6.1.4.1.171.10.37.40.2.1.1.1.1.2.1
-
 
  */
 ?>
